d977a5989102f488038f0f609a428d7ff0a8c600
dthatprince <nwaekwuprince@gmail.com>
2026-04-04 21:30:21 +0200

    fix deployment

--- backend/auth/__init__.py
+++ backend/auth/__init__.py
@@ -1,2 +1,12 @@
-from .security import verify_password, get_password_hash, create_access_token
-from .dependencies import get_current_user, require_admin, oauth2_scheme
+from .security import (
+    verify_password,
+    get_password_hash,
+    create_access_token,
+    create_refresh_token,
+)
+
+from .dependencies import (
+    get_current_user,
+    require_admin,
+    oauth2_scheme,
+)
